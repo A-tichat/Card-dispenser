3947cb4d85041c6380c3c331fab2c659fce97ba8
A-tichat <gamecup84@gmail.com>
2020-05-08 00:37:04 +0700

    push code in to github

---
+++ Error_checker.py
@@ -0,0 +1,142 @@
+class mrz:
+    TD="3"
+
+    def __init__(self,data):
+        #to check type of passport
+        if (data[:1] != "P"):
+            raise ValueError("There is another type")
+
+        #Ptype = data[1:2]
+
+        #to check country
+        self.country = data[2:5]
+        if not checkAlpha(self.country): raise ValueError("Picture is error!")
+
+        #to keep name 
+        self.name = ""
+        i=5
+        while True:
+            temp = data[i:i+1]
+            i+=1
+            if (temp == '\n'): break
+            if (temp == "<"):
+                temp += data[i:i+1]
+                i+=1
+                if (temp == "<\n" or temp == "<<"):
+                    break
+            self.name += temp
+        self.name = self.name.replace("<"," ")
+
+        #to keep surname
+        self.surname = ""
+        while True: 
+            temp = data[i:i+1]
+            i+=1
+            if (i>44 or temp == '\n'): break
+            if (temp == "<"):
+                temp += data[i:i+1]
+                i+=1
+                if (temp == "<\n" or temp == "<<"):
+                    break
+            self.surname += temp
+        self.surname = self.surname.replace("<"," ")
+
+        #to skip line
+        if not (data[i-1:i] == '\n'):
+            while True:
+                ch = data[i:i+1]
+                i+=1
+                if (ch == '\n'or i>44): 
+                    break
+
+        #to line 2 keep passport's number
+        self.passportNum = data[i:i+9]
+        i+=10
+        self.c_passportNum = data[i-1:i]
+        if not checkDigit(self.passportNum, self.c_passportNum): raise ValueError("Picture is error!")
+
+        #to keep nationality
+        self.nationality = data[i:i+3]
+        i+=3
+        if not checkAlpha(self.nationality): raise ValueError("Picture is error!")
+
+        #to keep dateOfBirth
+        self.dateOfBirth = data[i:i+6]
+        i+=7
+        self.c_dateOfBirth = data[i-1:i]
+        if not checkDigit(self.dateOfBirth, self.c_dateOfBirth): raise ValueError("Picture is error!")
+
+        #to keep sex
+        self.sex = data[i:i+1]
+        i+=1
+        if not checkAlpha(self.sex): raise ValueError("Picture is error!")
+        
+        #to keep expiration date of passport
+        self.expiry = data[i:i+6]
+        i+=7
+        self.c_expiry = data[i-1:i]
+        if not checkDigit(self.expiry, self.c_expiry): raise ValueError("Picture is error!")
+
+        #to keep Personal number
+        self.personalNum = data[i:i+14]
+        i+=15
+        self.c_personalNum = data[i-1:i]
+        if not checkDigit(self.personalNum, self.c_personalNum): raise ValueError("Picture is error!")
+
+        #to check digit for positions
+        last = data[i-22:i]+data[i-30:i-23]+data[i-43:i-33]
+        self.checksum = data[i:i+1]
+        i+=1
+        if not checkDigit(last, self.checksum): raise ValueError("Picture is error!")
+        print("Done")
+    
+    def printResult(self):
+        print("Type = "+self.TD)
+        print("Name = "+self.name)
+        print("Surname = "+self.surname)
+        print("Passport number = "+self.passportNum.replace("<",""))
+        print("Nationality = "+self.nationality)
+        yy = self.dateOfBirth[:2]
+        mm = self.dateOfBirth[2:4]
+        dd = self.dateOfBirth[4:]
+        print("Date of birth = "+yy+" "+mm+" "+dd)
+        print("Sex = "+self.sex)
+        yy = self.expiry[:2]
+        mm = self.expiry[2:4]
+        dd = self.expiry[4:]
+        print("Expiration date = "+yy+" "+mm+" "+dd)
+        print("Personal number = "+self.personalNum.replace("<",""))
+
+def checkDigit(data,checker):
+    cnum=0
+    posi=0
+    size = len(data)
+    if not checker.isdigit(): return False
+    for n in data:
+        value = 0
+        if (n.isalpha()):
+            value = ord(n)-55
+        elif (n.isdigit()):
+            value = int(n)
+        elif (n == '<'):
+            value = 0
+        else:
+            return False
+        if(posi%3 == 0):
+            cnum += value*7
+        elif(posi%3 == 1):
+            cnum += value*3
+        elif(posi%3 == 2):
+            cnum += value
+        posi+=1
+    if (int(checker) == cnum%10): return True
+    else:
+        return False
+
+def checkAlpha(checker):
+    if checker=="D<<": return True
+    for c in checker:
+        if c == " ": continue
+        if not c.isalpha(): return False
+    return True
+
---
+++ main.py
@@ -0,0 +1,92 @@
+import asyncio
+import logging
+import random
+import time
+
+from nextion import Nextion, EventType
+
+password = 'l'
+
+async def checkKey():
+    try:
+        key = await client.get('t0.txt')
+        key = key[1:]
+        if (key.isdigit()):
+            print('True')
+            await client.command('page 4')
+            await asyncio.sleep(0.3)
+            for i in range(10, 0, -1):
+                await client.set('p4_n0.val', i)
+                await asyncio.sleep(1)
+            await client.command('page 6')
+            await asyncio.sleep(0.3)
+            await client.set('n0.val', int(key))
+            for i in range(10, 0, -1):
+                await client.set('p4_n0.val', i)
+                if (await client.get('dp') == 1):
+                    print('back is press')
+                    break
+                elif (i == 1):
+                    await asyncio.sleep(1)
+                    await client.command('page 1')
+                    break
+                await asyncio.sleep(1)
+        else:
+            print('False')
+            await client.set('t0.txt', ' ')
+    except NameError:
+        print('checkKey function: ', NameError)
+
+async def caplock():
+    try:
+        temp = await client.get('t0.txt')
+        print(temp)
+        if (await client.get('dp') == 2):
+            await client.command('page 3')
+        elif (await client.get('dp') == 3):
+            await client.command('page 2')
+        else:
+            print('Error: duplicate key ID 38 or 40')
+        await asyncio.sleep(0.3)
+        await client.set('t0.txt',temp)
+    except NameError:
+        print('caplock function: ',NameError)
+    
+
+def event_handler(type_, data):
+    if type_ == EventType.STARTUP:
+        print('We have booted up!')
+    elif type_ == EventType.TOUCH:
+        if data.component_id == 42:
+            asyncio.create_task(checkKey())
+        elif data.component_id == 40 or data.component_id == 38:
+            asyncio.create_task(caplock())
+    logging.info('Data: '+str(data))
+
+async def run():
+    global client
+    client = Nextion('/dev/ttyAMA0', 9600, event_handler)
+    await client.connect()
+
+    # await client.sleep()
+    await client.wakeup()
+    
+    #await client.command('sendxy=0')
+    await client.command('page 0')
+    await asyncio.sleep(1.5)
+
+    if (await client.get('dp') != 1):
+        await client.command('page 1')
+
+    print('finished')
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.DEBUG,
+        handlers=[
+            logging.StreamHandler()
+        ])
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(run())
+    loop.run_forever()
