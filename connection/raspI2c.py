463e01f4c151c01ee3a9a0c1e095e2c930c5a2fa
A-tichat <gamecup84@gmail.com>
2020-05-08 09:23:08 +0700

    add code I2c from another pi

--- connection/raspI2c.py
+++ connection/raspI2c.py
@@ -1,1 +1,32 @@
-print "Hello World"
+
+#  Raspberry Pi Master for Arduino Slave
+#  i2c_master_pi.py
+#  Connects to Arduino via I2C
+  
+#  DroneBot Workshop 2019
+#  https://dronebotworkshop.com
+ 
+from smbus import SMBus
+from time import sleep
+import binascii
+ 
+addr = 0x4 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
+ 
+numb = 1
+ 
+print ("Enter 1 for ON or 0 for OFF")
+while numb == 1:
+ 	try:
+		for i in range(0,5):
+			sleep(2)
+			block = bus.read_i2c_block_data(addr, 0)
+			print(block)
+			
+	except KeyboardInterrupt:
+		data = raw_input(">>>>   ")
+		charString = []
+		for character in data:
+			charString.append(ord(character))
+		bus.write_i2c_block_data(addr, 0, charString) 
+		
