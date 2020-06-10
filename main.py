03579c7bf08be80f7d7e20b08bc5765c7a16c8db
a-tichat <gamecup84@gmail.com>
2020-06-10 15:55:15 +0700

    nextion not support thai lang

--- main.py
+++ main.py
@@ -130,8 +130,7 @@ async def findId():
     temp = cardScan()
     print(temp)
     await client.command('xstr 200,230,400,30,1,BLACK,WHITE,0,0,1,"CID: %s"' % temp.cid)
-    await client.command('xstr 200,200,400,30,1,BLACK,WHITE,0,0,1,"TH FullnName: %s"' % temp.thfullname)
-    await client.command('xstr 200,290,400,30,1,BLACK,WHITE,0,0,1,"Address: %s"' % temp.addr)
+    await client.command('xstr 200,200,400,30,1,BLACK,WHITE,0,0,1,"TH FullnName: %s"' % temp.enfullname)
     rooms = getRoom('cid', temp.cid)
     if rooms:
         stm32.sendSlot(rooms)
