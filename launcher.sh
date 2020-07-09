546f6a4a785fc83e53d156c7aca0fec5755f9044
A-tichat <gamecup84@gmail.com>
2020-07-09 20:24:31 +0700

    done start program at pi startup

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,8 @@
-!/bin/sh
+#!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
 cd home/pi/Card-dispenser
-source bin/activate
 sudo python3 main.py
 cd /
