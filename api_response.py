40d494c80a5609aa92e8e25b9e010f0f60145210
a-tichat <gamecup84@gmail.com>
2020-07-13 15:56:47 +0700

    new branch for internet problem

--- api_response.py
+++ api_response.py
@@ -1,13 +1,20 @@
 import requests
 import json
-import urllib.request
+
 
 def postAPI(action, json_data):
-    url = "http://kds.nellehliving.com/api/v1/"
-    api_key = "44d1836c5185a5c52e5a8f428e603f6e"
-    api_params = {'action':action, 'KEY':api_key}
-    response = requests.post(url, json=json_data, params=api_params)
-    return response
+    try:
+        url = "http://kds.nellehliving.com/api/v1/"
+        api_key = "44d1836c5185a5c52e5a8f428e603f6e"
+        api_params = {'action': action, 'KEY': api_key}
+        response = requests.post(
+            url, json=json_data, params=api_params, timeout=10)
+        return response.text
+    except requests.Timeout:
+        return 'timeout'
+    except requests.ConnectionError:
+        return 'connectionError'
+
 
 def getRoom(mytype, value):
     switcher = {
@@ -16,17 +23,14 @@ def getRoom(mytype, value):
         "password": "3",
         "fullname": "4"
     }
-    json_data = {'type':switcher.get(mytype, '0'), 'value':value}
-    return json.loads(postAPI('check', json_data).text)
-    
+    json_data = {'type': switcher.get(mytype, '0'), 'value': value}
+    res = postAPI('check', json_data)
+    if (res == 'timeout' or res == 'connectionError'):
+        return json.loads("")
+    else:
+        return json.loads(res)
+
+
 def resetRoom(arr_data):
     res = postAPI('return', arr_data)
     print(res.text)
-
-# this function to check raspberry pi can connect to network
-def connect(host='http://google.com'):
-    try:
-        urllib.request.urlopen(host)  # Python 3.x
-        return True
-    except:
-        return False
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import time
 import threading
 import re
+import socket
 
 import pytesseract
 import RPi.GPIO as GPIO
@@ -19,26 +20,76 @@ from mrzScanWithABBYY import scanMrzWithPi
 from thaiId import cardScan
 from api_response import *
 
+
+def internet_status(host="8.8.8.8", port=53, timeout=3):
+    try:
+        global netStat
+        global netCheck, connect
+        time.sleep(3)
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        connect = True
+        if (netStat < 1):
+            time.sleep(1)
+            asyncio.create_task(change_stat())
+            netStat = 2
+        if (netCheck):
+            internet_status()
+    except socket.error as ex:
+        connect = False
+        if (netStat > 1):
+            asyncio.create_task(change_stat())
+            netStat = 0
+        if (netCheck):
+            internet_status()
+
+
+async def change_stat():
+    global connect
+    if connect:
+        await client.command('status.pic=17')
+    else:
+        await client.command('status.pic=16')
+
+
+def connection(host="8.8.8.8", port=53, timeout=1):
+    try:
+        global netStat
+        global connect
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        connect = True
+        change_stat()
+        netStat = 2
+    except socket.error as ex:
+        print(ex)
+        connect = False
+        change_stat()
+        netStat = 0
+
+
 # this function will receive PIN-code from nextion display
 # then check to server get room_number and slot
 # then match slot to stm32 address and send many slot to stm32
-
-
 async def checkKey():
     try:
+        global client
+        global connect
         getkey = await client.get('t0.txt')
         await client.command('page waiting_page')
         PIN = re.sub(' ', '', getkey)
 
         # check internet connection
-        if connect() and PIN:
+        if connect and PIN:
             rooms = getRoom('password', PIN)
         else:
             await client.command('page pageWrong')
-            await client.set('p6_t0', "Can't connect to internet")
-            await client.set('p6_t1', "Make sure this device connect to internet correctly")
+            await client.set('p6_t0.txt', "Can't connect to internet")
+            await client.set('p6_t1.txt', "Make sure this device connect to internet correctly")
             await client.command('tm0.en=0')
 
+        if (await client.get('dp') != 9):
+            raise NameError("cancel press")
         # check pin correct
         if rooms:
             stm32.sendSlot(rooms)
@@ -68,8 +119,8 @@ async def scanPassport():
 # then check that to server get number room
 # then send key slot to stm32
 async def checkPassport(path, camera):
-    global client
     try:
+        global client
         GPIO.output(17, GPIO.HIGH)
         noPP = True
         while noPP:
@@ -87,12 +138,12 @@ async def checkPassport(path, camera):
         GPIO.output(17, GPIO.LOW)
 
         # check internet connection
-        if connect():
+        if connect:
             data = scanMrzWithPi(path)
         else:
             await client.command('page pageWrong')
-            await client.set('p6_t0', "Can't connect to internet")
-            await client.set('p6_t1', "Make sure this device connect to internet correctly")
+            await client.set('p6_t0.txt', "Can't connect to internet")
+            await client.set('p6_t1.txt', "Make sure this device connect to internet correctly")
             await client.command('tm0.en=0')
         personNum = data.personalNum.replace("<", "")
 
@@ -114,12 +165,6 @@ async def checkPassport(path, camera):
         print(e)
     except ValueError:
         await client.command('page pageWrong')
-
-
-async def scanId():
-    global client
-    await client.command('page idScan')
-    await findId()
 
 
 async def findId():
@@ -154,13 +199,21 @@ def event_handler(type_, data):
     if type_ == EventType.STARTUP:
         print('We have booted up!')
     if type_ == EventType.TOUCH:
-        if (data.page_id == 3 and data.component_id == 42 or data.page_id == 2 and data.component_id == 41):
-            asyncio.create_task(checkKey())
-        elif (data.page_id == 4 and data.component_id == 3):
-            asyncio.create_task(scanPassport())
-        elif (data.page_id == 4 and data.component_id == 2):
-            asyncio.create_task(scanId())
-    logging.info('Data: '+str(data))
+        if (data.page_id == 1):
+            if (data.component_id == 3):
+                connection()
+        elif (data.page_id == 2):
+            if (data.component_id == 41):
+                asyncio.create_task(checkKey())
+        elif (data.page_id == 3):
+            if (data.component_id == 42):
+                asyncio.create_task(checkKey())
+        elif (data.page_id == 4):
+            if (data.component_id == 3):
+                asyncio.create_task(scanPassport())
+            elif (data.component_id == 2):
+                asyncio.create_task(findId())
+    # logging.info('Data: '+str(data))
 
 
 # initial nextion function
@@ -176,30 +229,36 @@ async def run():
     await client.command('page 0')
     await asyncio.sleep(1.5)
 
-    if connect():
+    if connect:
         if (await client.get('dp') != 1):
             await client.command('page stb_page')
     else:  # if connect to internet fail!
         await client.command('page pageWrong')
-        await client.set('p6_t0', "Can't connect to internet")
-        await client.set('p6_t1', "Make sure this device connect to internet correctly")
+        await client.set('p6_t0.txt', "Can't connect to internet")
+        await client.set('p6_t1.txt', "Make sure this device connect to internet correctly")
         await client.command('tm0.en=0')
     print('finished')
 
 
 # main function
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.setup(27, GPIO.OUT)
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.HIGH)
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level=logging.DEBUG,
-        handlers=[
-            logging.StreamHandler()
-        ])
-    loop = asyncio.get_event_loop()
-    asyncio.ensure_future(run())
-    loop.run_forever()
+    try:
+        netStat = 0
+        connect = True
+        netCheck = True
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(17, GPIO.OUT)
+        GPIO.output(17, GPIO.LOW)
+        checkNetwork = threading.Thread(target=internet_status)
+        logging.basicConfig(
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            level=logging.DEBUG,
+            handlers=[
+                logging.StreamHandler()
+            ])
+        loop = asyncio.get_event_loop()
+        asyncio.ensure_future(run())
+        checkNetwork.start()
+        loop.run_forever()
+    finally:
+        netCheck = False
